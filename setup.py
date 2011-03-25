5af979d5d245a647df30e1c7017e9663a6cbc4de
plotnik <plotnikoff@gmail.com>
2011-03-25 09:48:08 +0700

    Project's meta info update

--- setup.py
+++ setup.py
@@ -8,11 +8,18 @@ setup(name='auth_pubtkt',
       description="This software implements mod_auth_pubtkt authentication for Python world.",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Programming Language :: Python',
+      'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware'
+      ],
       keywords='mod_auth_pubtkt authentication single-sing-on',
       author='Andrey Plotnikov',
       author_email='plotnikoff@gmail.com',
-      url='',
+      url='https://github.com/AndreyPlotnikov/auth_pubtkt',
       license='BSD License',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
